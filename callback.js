fe83143c165ba5a8a18a476bb4941653283f64f2
Kingbunner <raynaldjonene@gmail.com>
2024-09-21 21:54:06 +0800

    Update callback.js

--- callback.js
+++ callback.js
@@ -2,7 +2,3 @@ function getQueryParam(param) {
     let params = new URLSearchParams(window.location.search);
     return params.get(param);
 }
-
-// Example usage:
-let name = getQueryParam('name');
-console.log(name); // Will log the value of the 'name' query parameter if present.
