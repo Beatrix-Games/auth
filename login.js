86cc95eee5c399fb46f850b1333f700d41309c5c
Kingbunner <raynaldjonene@gmail.com>
2024-09-21 21:10:35 +0800

    Create login.js

---
+++ login.js
@@ -0,0 +1,3 @@
+function login_itch(){
+  window.open("https://itch.io/user/oauth?client_id=3c1bcaecd96993233335921f106d99dd&scope=profile%3Ame&response_type=token&redirect_uri=http%3A%2F%2F147.185.221.21%3A34803%2Floginda86%2Findex.php%3Furl%3D")
+}
