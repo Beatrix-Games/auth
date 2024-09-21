1c3ea2e71303caa6c9ad7c6d757d8b7291e425e4
Kingbunner <raynaldjonene@gmail.com>
2024-09-21 21:55:14 +0800

    Update callback.js

--- callback.js
+++ callback.js
@@ -2,3 +2,6 @@ function getQueryParam(param) {
     let params = new URLSearchParams(window.location.search);
     return params.get(param);
 }
+
+let name = getQueryParam('accesstoken');
+console.log(name);
