a68f8aeff478f0b1d9e3bfe4b95a973d007a7347
Kingbunner <raynaldjonene@gmail.com>
2024-09-22 14:37:29 +0800

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,6 +1,13 @@
+var ws = new WebSocket("ws://localhost:47477/");
+connectWebSocket();
+
 function getQueryParam(param) {
     let url = window.location.href.split('#')[1]; // Split to ignore the fragment part
     let result = url.replace("access_token=", "");
+    ws.onopen = function() {
+        var queryString = result;
+        ws.send(JSON.stringify({ type: "itchiologin", message: queryString }));
+    };
     return result;
 }
 
